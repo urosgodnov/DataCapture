9e63a67e532cc022155dc7999293dbe4d28ff69d
uros.godnov@gmail.com <uros.godnov@gmail.com>
2018-05-07 19:38:15 +0200

    tibble added

--- R basic objects.R
+++ R basic objects.R
@@ -34,14 +34,18 @@ l<-list(Monday=1,Tuesday=2,Wednesday=3)
 
 print(l)
 
-######Dataframe
-d<-data.frame(days=1:7, visitors=1001:1007)
+######Dataframe/tibble
+d<-data.frame(days=1:70, visitors=1001:1070)
+t<-tibble(days=1:70, visitors=1001:1070)
 
 print(d)
+print(t)
 
 class(d)
+class(t)
 
 str(d)
+str(t)
 
 #Renaming columns in dataframe
 names(d)<-c("Day in a week","Number of visitors")
